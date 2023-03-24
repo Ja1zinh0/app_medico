370743a3c3e2f7ea86358e854b74c20390aac8cc
Joao Henrique <joao2011bha@hotmail.com>
2023-03-24 20:35:28 -0300

    third commit

--- lib/view/tela_login.dart
+++ lib/view/tela_login.dart
@@ -1,3 +1,4 @@
+import 'package:app_prototipo/view/tela_recSenha.dart';
 import 'package:flutter/material.dart';
 
 class TelaLogin extends StatefulWidget {
@@ -24,7 +25,16 @@ class _TelaLoginState extends State<TelaLogin> {
               SizedBox(
                 width: MediaQuery.of(context).size.width - 100,
                 child: Column(
+
                   children: [
+                    
+                    const SizedBox(
+                      child: Icon(
+                        Icons.account_circle, 
+                        size: 200,
+                        ),
+                    ),
+
                     TextField(
                       decoration: InputDecoration(
                       contentPadding: const EdgeInsets.all(2),
@@ -56,8 +66,42 @@ class _TelaLoginState extends State<TelaLogin> {
 
                 // ignore: prefer_const_constructors
                 SizedBox(
-                  height: 40,
+                  height: 5,
                 ),
+                
+                Column(
+                  children: [
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RecuperarSenha(),
+                              ),
+                            );
+                          },
+                            child: 
+                              const Text('Esqueceu a senha?',
+                              style: TextStyle(
+                                color: Colors.black,
+                                decoration: TextDecoration.underline,
+                              ),
+                              )
+                         ),
+                      ]
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
                 SizedBox(
                   width: 160,
                   height: 32,
@@ -68,14 +112,13 @@ class _TelaLoginState extends State<TelaLogin> {
                         borderRadius: BorderRadius.circular(30.0),
                       ),
                     ),
-
                     onPressed: (){
                 
                     },
-                
                     child: const Text('Entrar'),
                   ),
-                )
+                ),
+
                 ],
                 )
               )
